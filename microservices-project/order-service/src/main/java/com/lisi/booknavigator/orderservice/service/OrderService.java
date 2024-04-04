750aa9b3120d32112dca051c52f8db022d92ba91
lisiCAO <caolisi@hotmail.com>
2024-04-04 11:12:03 -0400

    foundamental structures

---
+++ microservices-project/order-service/src/main/java/com/lisi/booknavigator/orderservice/service/OrderService.java
@@ -0,0 +1,86 @@
+package com.lisi.booknavigator.orderservice.service;
+
+import com.lisi.booknavigator.orderservice.dto.InventoryResponse;
+import com.lisi.booknavigator.orderservice.dto.OrderLineItemsDto;
+import com.lisi.booknavigator.orderservice.dto.OrderRequest;
+import com.lisi.booknavigator.orderservice.event.OrderPlacedEvent;
+import com.lisi.booknavigator.orderservice.model.Order;
+import com.lisi.booknavigator.orderservice.model.OrderLineItems;
+import com.lisi.booknavigator.orderservice.reponsitory.OrderRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
+    public String placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDomain)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+        log.info("Calling inventory service");
+
+        Span inventoryServiceLookup = tracer.nextSpan().name("inventoryServiceLookup");
+        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+            // Calling Inventory Service.
+            // stock
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory", uriBuilder ->
+                            uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            assert inventoryResponseArray != null;
+            boolean AllProductsInStock = Arrays.stream(inventoryResponseArray)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if (AllProductsInStock) {
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                log.info("Order {} is saved", order.getId());
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+                // TODO: custom exception-ProductOutOfStockException
+            }
+        } finally {
+            inventoryServiceLookup.end();
+        }
+    }
+
+    private OrderLineItems mapToDomain(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
