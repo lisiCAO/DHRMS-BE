0d708dec20a4cfeff8c014fe6d36f7f8107775e5
lisiCAO <caolisi@hotmail.com>
2024-04-16 08:03:27 -0400

    add cors configuration

--- microservices-project/api-gateway/src/main/java/com/lisi/booknavigator/apigateway/config/SecurityConfig.java
+++ microservices-project/api-gateway/src/main/java/com/lisi/booknavigator/apigateway/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.lisi.booknavigator.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,6 +20,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
 
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeExchange(exchanges -> exchanges
                         .anyExchange().permitAll());  // 允许所有请求 Test only
 //                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));  // use Default explicitly 6.1
@@ -24,4 +30,17 @@ public class SecurityConfig {
                  */
         return serverHttpSecurity.build();
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8000")); // 允许的源
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 允许的方法
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type")); // 允许的头部
+        corsConfig.setAllowCredentials(true); // 允许证书
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // 对所有路径应用此CORS配置
+        return source;
+    }
+
 }
