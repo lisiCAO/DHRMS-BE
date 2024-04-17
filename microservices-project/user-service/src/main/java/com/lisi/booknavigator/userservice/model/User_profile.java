5be2efc4181db76ab2c4c584eafce3b2c1bd338b
tech8228 <ashok.tech8228@gmail.com>
2024-04-16 21:58:45 -0400

    foru

--- microservices-project/user-service/src/main/java/com/lisi/booknavigator/userservice/model/User_profile.java
+++ microservices-project/user-service/src/main/java/com/lisi/booknavigator/userservice/model/User_profile.java
@@ -1,4 +1,18 @@
 package com.lisi.booknavigator.userservice.model;
 
-public class UserProfile {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User_profile {
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private String address;
+    private String postcode ;
+    private Long imagefieldid;
 }
+
