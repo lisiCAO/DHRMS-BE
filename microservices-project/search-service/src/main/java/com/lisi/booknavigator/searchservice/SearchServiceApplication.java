662d38c07a4343cc47e15d43eb3eb7d770f89b73
tech8228 <ashok.tech8228@gmail.com>
2024-04-17 22:48:59 -0400

    hotfixsearch

--- microservices-project/search-service/src/main/java/com/lisi/booknavigator/searchservice/SearchServiceApplication.java
+++ microservices-project/search-service/src/main/java/com/lisi/booknavigator/searchservice/SearchServiceApplication.java
@@ -1,11 +1,31 @@
 package com.lisi.booknavigator.searchservice;
 
+import com.lisi.booknavigator.searchservice.service.ElasticsearchService;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+
 @SpringBootApplication
 public class SearchServiceApplication {
+    private static final Logger log = LoggerFactory.getLogger(SearchServiceApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SearchServiceApplication.class, args);
+        //SpringApplication.run(SearchServiceApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(SearchServiceApplication.class, args);
+
+        // Retrieve the ElasticsearchService bean
+        ElasticsearchService elasticsearchService = context.getBean(ElasticsearchService.class);
+
+        // Reload all properties to Elasticsearch
+        elasticsearchService.reloadProperties();
     }
 }
